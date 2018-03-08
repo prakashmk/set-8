598a4c86230fb6526776c93c585de8361fbde17d
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-03-08 16:05:39 +0530

    Create p71.c

---
+++ p71.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+int main ()
+{ 
+char s[20];
+int len, i, flag = 0;
+printf ("Enter the string:");
+scanf ("%s", s);
+len = strlen (s);
+for (i = 1; i <= len; i++)
+{
+ if (s[i] == s[len - i - 1])
+	flag = flag + 1;
+ }
+  if (flag == len)
+      printf ("It is palindrome");
+  else
+    printf ("It is not palindrome");
+  return 0;
+}
