58c81991058be4cd691879380c670462adb8a78f
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-03-08 16:23:05 +0530

    Create p72.c

---
+++ p72.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+
+int main ()
+{
+  char s[20];
+  int i;
+  printf ("Enter the string:");
+  scanf ("%s", s);
+if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+   printf ("It is vowel");
+else
+   printf ("It is not Vowel");
+  return 0;
+}
